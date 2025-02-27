5e68f82443a5459ffb23a169dac160e4b25f8a85
Etainos <yang-rz20@tsinghua.org.cn>
2025-02-27 20:56:35 +0000

    Create creat_table.py

---
+++ team_jacaranda/coursework_one/modules/creat_table.py
@@ -0,0 +1,42 @@
+import psycopg2
+from psycopg2 import sql
+
+# PostgreSQL database connection configuration
+db_config = {
+    "dbname": "fift",
+    "user": "postgres",
+    "password": "postgres",
+    "host": "host.docker.internal",
+    "port": 5439
+}
+
+# SQL query to create a new table
+create_table_query = """
+CREATE TABLE IF NOT EXISTS csr_reporting.csr_reports (
+    id SERIAL PRIMARY KEY,
+    security TEXT,
+    report_url VARCHAR(255),
+    report_year INTEGER,
+    minio_path VARCHAR(255)
+)
+"""
+
+# Connect to PostgreSQL database and create new table
+try:
+    # Connect to the database
+    conn = psycopg2.connect(**db_config)
+    cursor = conn.cursor()
+
+    # Execute the SQL query to create the table
+    cursor.execute(create_table_query)
+
+    # Commit the transaction
+    conn.commit()
+    print("Table csr_reporting.csr_reports created successfully (if not exists)!")
+except Exception as e:
+    print(f"An error occurred: {e}")
+finally:
+    # Close the database connection
+    if conn:
+        cursor.close()
+        conn.close()
