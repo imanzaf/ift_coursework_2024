5737d1ef29409d35dbd484053750a718b20ef5c5
Esther Agossa <estheragossa@gmail.com>
2025-02-28 02:10:16 +0000

    comments

--- team_sakura/coursework_one/a_pipeline/modules/db_loader/mongo_db.py
+++ team_sakura/coursework_one/a_pipeline/modules/db_loader/mongo_db.py
@@ -2,30 +2,40 @@ import yaml
 from pymongo import MongoClient
 import os
 
-config_path = os.getenv(
-    "CONF_PATH", "a_pipeline/config/conf.yaml"
-)  # Default path for Docker
+# Load configuration from YAML file, using a default path for Docker if not set in environment variables
+config_path = os.getenv("CONF_PATH", "a_pipeline/config/conf.yaml")
 with open(config_path, "r") as file:
     config = yaml.safe_load(file)
 
-# Determine if running in Docker
+# Determine MongoDB configuration based on environment (Docker or local)
 if os.getenv("DOCKER_ENV"):
     mongo_config = config["databasedocker"]
 else:
     mongo_config = config["databaselocal"]
 
 
-# MongoDB Connection
 def get_mongo_collection():
-    MONGO_CLIENT = MongoClient(mongo_config["mongo_uri"])
-    db = MONGO_CLIENT[mongo_config["mongo_db"]]
+    """
+    Establish a connection to MongoDB and return the specified collection.
+
+    Returns:
+        pymongo.collection.Collection: The MongoDB collection object.
+    """
+    mongo_client = MongoClient(mongo_config["mongo_uri"])
+    db = mongo_client[mongo_config["mongo_db"]]
     collection = db[mongo_config["mongo_collection"]]
+
+    # Create indexes to optimize queries on `company_name` and `report_year`
     collection.create_index([("company_name", 1)])
     collection.create_index([("report_year", 1)])
+
     return collection
 
 
 def delete_all_documents_from_mongo():
+    """
+    Deletes all documents from the specified MongoDB collection.
+    """
     collection = get_mongo_collection()
     try:
         # Delete all documents in the collection
@@ -35,4 +45,4 @@ def delete_all_documents_from_mongo():
         print(f"Error while deleting from MongoDB: {e}")
 
 
-# delete_all_documents_from_mongo() Call the function to delete all documents from MongoDB
+# Call delete_all_documents_from_mongo() to remove all documents from MongoDB when needed
